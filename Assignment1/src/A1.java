f6778ecac552e66ad3e66c8a326dcad58bfdd12c
parmeetgill1313 <32675943+parmeetgill1313@users.noreply.github.com>
2017-11-21 08:50:50 -0600

    Add files via upload

---
+++ Assignment1/src/A1.java
@@ -0,0 +1,122 @@
+/********************************************************************
+ * Programmer:      Parmeet Gill
+ * Class:           CS30S
+ *
+ * Assignment:      A1 Q1
+ * Program Name:    Assignment 1
+ *
+ * Description:     prints a list of between 950 and 1000 random numbers 
+ *                  between 1 and 100 to a file 
+ *
+ * Input:           none
+ *
+ * Output:          950-1000 random numbers
+ ***********************************************************************/
+ 
+ // import java libraries here as needed
+ 
+ import javax.swing.*;
+ import java.text.DecimalFormat;
+ import java.io.*; 			// import file io libraries
+import java.util.Random;
+ 
+public class A1 {  // begin class
+    
+    public static void main(String[] args) throws IOException{  // begin main
+    
+    // ********* declaration of constants **********
+        
+        final int max = 100;            //maximum for the random # generated
+        final int MAXSIZE = 1000;       //amount of random #s generated
+        
+    // ********** declaration of variables **********
+    
+    	String delim = "[ ]+";		// delimiter string for splitting input string
+        
+    	int randNum;                    //int for random numbers
+        int list[] = new int[MAXSIZE];      //array of ints
+        
+    // create instances of objects for i/o and formatting
+    
+    	PrintWriter fout = new PrintWriter(new BufferedWriter(new FileWriter("testOut.txt")));
+        Random rand = new Random();     //random number generator
+        
+    // ********** Print output Banner **********
+    
+        printBanner();      //prints banner to output
+        fileBanner(fout);   //prints banner to file
+    
+    // ************************ get input **********************
+    
+        for (int n = 0; n < MAXSIZE; n++){//start for loop 
+            list[n] = rand.nextInt(max) + 1; 
+            //generated random numbers between 1-100
+            //list[n] = randNum;  //array of random numbers
+           // count++;        //# counter
+           // fout.println(randNum);  //prints #s in the file
+           // System.out.println(randNum);    //prints #s in the console
+        }//end int random for loop 
+        
+    // ************************ processing ***************************
+    
+    // ************************ print output ****************************
+
+        fileList(list, MAXSIZE, fout);
+        // System.out.println("\nThere are: " + count + " numbers in the array.");
+       //  fout.println("\nThere are: " + count + " numbers in the array.");
+
+    // ******** closing message *********
+        
+        System.out.println("\nend of processing.");
+        fout.format("end of processing.");
+   
+    // ***** close streams *****
+        
+        //fin.close();			// close input buffer
+        fout.close();			// close output buffer
+        
+    }  // end main
+    
+    //************************************************
+    // Purpose: print an output banner to the output file
+    // Interface: IN: PrintWriter
+    // Returns: none
+    // *****************************************************
+    public static void fileBanner(PrintWriter fo){
+        fo.println("*******************************************");
+        fo.println("Name:		Parmeet Gill");
+        fo.println("Class:		CS30S");
+        fo.println("Assignment:         Assignment 1 Q1");
+        fo.println("*******************************************");    
+    } // end fileBanner
+    
+    //************************************************
+    // Purpose: print an output banner to the output window
+    // Interface: IN: none
+    // Returns: none
+    // *****************************************************
+    public static void printBanner(){
+        System.out.println("*******************************************");
+        System.out.println("Name:		Parmeet Gill");
+        System.out.println("Class:		CS30S");
+        System.out.println("Assignment:         Assignment 1 Q1");
+        System.out.println("*******************************************");
+    }  // end print banner
+    
+    //************************************************
+    // Purpose: print the array to the file
+    // Interface: IN: base adress of list,
+    //                actual size of list
+    // Returns: none
+    // *****************************************************
+    public static void fileList(int[] l, int n, PrintWriter fout){
+       int count = 0;   //counter
+       for (int i = 0; i < n; i++){
+           fout.println(l[i]);  //prints the random numbers 
+           count++;     //counts the #s
+       }
+            fout.println("\nThere are: " + count + " numbers in the array.");
+
+    }  // end print list
+    
+}  // end class
